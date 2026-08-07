c257af8439c414f8096bb6b703d91eb03dc7fe0c
Kavya Mullapudi <kavyaamullapudi.11@gmail.com>
2026-08-07 14:39:41 +0530

    Improve scrolltotp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+
 function App() {
   return (
     <BrowserRouter>
