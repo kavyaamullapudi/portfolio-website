d9a630228ad2471050bd805fd770fbece9a72ab8
Kavya Mullapudi <kavyaamullapudi.11@gmail.com>
2026-08-07 12:53:28 +0530

    Improve footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
         </a>
 
         <a
-          href="https://www.linkedin.com/in/kavya-mullapudi-8588b6380"
+          href="https://www.linkedin.com/in/kavya-mullapudi-8588b6380/"
           target="_blank"
           rel="noopener noreferrer"
         >
