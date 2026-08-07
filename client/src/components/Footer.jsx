49ef6f36b8bb2117f653db37097b85988e7e40ed
Kavya Mullapudi <kavyaamullapudi.11@gmail.com>
2026-08-07 12:39:43 +0530

    Improve navbar design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,27 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>© 2026 Kavya. All Rights Reserved.</p>
+
+      <div className="footer-links">
+        <a
+          href="https://github.com/kavyaamullapudi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+
+        <a
+          href="www.linkedin.com/in/kavya-mullapudi-8588b6380
+
+"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
     </footer>
   );
 }
